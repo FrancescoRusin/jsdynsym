aeaa849f49b43622172076ac877ed9f94e525aa1
Francesco <rusinfrank98@gmail.com>
2026-08-18 05:03:31 +0200

    Neural actor and critic operational.

---
+++ io.github.ericmedvet.jsdynsym.core/src/main/java/io/github/ericmedvet/jsdynsym/core/rl_2/RLEpisodicTask.java
@@ -0,0 +1,9 @@
+package io.github.ericmedvet.jsdynsym.core.rl_2;
+
+import io.github.ericmedvet.jnb.datastructure.Pair;
+
+import java.util.function.Consumer;
+
+public interface RLEpisodicTask<S, O>{
+    <P> Pair<P, O> runEpisode(RLMethod<P> method, int timesteps, Consumer<S> listener);
+}
